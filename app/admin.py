5233bd622107973ab18a87d1604a5d4b9a22972b
stevanmatovic <stevan.matovic95@gmail.com>
2018-08-21 00:35:29 +0200

    base models for testing

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import User, Product, Exchange
+
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Exchange)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50,unique=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=2000)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Exchange(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=2000)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
